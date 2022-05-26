63e0715bc8a354e8eaed9e3e29b88f3d50dc24c8
Pablo Dellacassa <pdellacassa@gmail.com>
2022-05-26 15:53:33 +0200

    Update materials

--- src/components/IronbnbList.js
+++ src/components/IronbnbList.js
@@ -4,29 +4,45 @@ import axios from "axios";
 const apiURL = "https://ironbnb-m3.herokuapp.com/apartments";
 
 function IronbnbList() {
-  const [fetching, setFetching] = useState(true);
+  const [loading, setFetching] = useState(true);
+  const [waiting, setWaiting] = useState(true);
   const [apartments, setApartments] = useState([]);
 
   useEffect(() => {
-    console.log("useEffect - Initial render (Mounting)");
+    //console.log("useEffect - Initial render (Mounting)");
     axios.get(apiURL).then((response) => {
       setApartments(response.data);
       setFetching(false);
     });
   }, []);
 
+  useEffect(() => {
+    // set a timer that after 2 seconds displays the list of appartments
+    const id = setTimeout(() => {
+      setWaiting(false);
+    }, 2000);
+    return () => {
+      clearInterval(id);
+      console.log("Component Unmounting");
+    };
+  }, []);
+
+
+
   return (
     <div>
       <h3>List of apartments</h3>
-      {fetching && <p>Loading ...</p>}
-
-      {apartments.map((apt) => (
-        <div key={apt._id} className="card">
-          <img src={apt.img} alt="apartment" />
-          <h3>{apt.title}</h3>
-          <p>Price: {apt.pricePerDay}</p>
-        </div>
-      ))}
+      {loading || waiting ? (
+        <p>Loading ...</p>
+      ) : (
+        apartments.map((apt) => (
+          <div key={apt._id} className="card">
+            <img src={apt.img} alt="apartment" />
+            <h3>{apt.title}</h3>
+            <p>Price: {apt.pricePerDay}</p>
+          </div>
+        ))
+      )}
     </div>
   );
 }
